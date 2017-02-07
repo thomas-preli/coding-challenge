1b623b5ffa8327e43ad71e97ca306bea0c4239e0
Thomas Preli <thomaspreli@Thomass-MacBook-Pro.local>
2017-02-07 01:05:09 -0600

    master: Symbol lookup drop down changes

--- app/components/symbol-lookup.js
+++ app/components/symbol-lookup.js
@@ -2,14 +2,21 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   ajax: Ember.inject.service(),
+  symbols: [],
 
   actions: {
-    searchRepo(term) {
-      let url = `https://api.github.com/search/repositories?q=${term}`;
-      return this.get('ajax').request(url).then((json) => json.items);
+    checkLength(text, select) {
+      if (select.searchText.length >= 3 && text.length < 3) {
+        this.set('symbols', []);
+        return false;
+      } else {
+        return text.length >= 3;
+      }
     },
-    symbolChange(stockSymbol) {
-      this.set('selectedStockSymbol', stockSymbol);
+
+    searchRepo(searchTerm) {
+      const url = `http://localhost:8080/symbols?searchTerm=${searchTerm}`;
+      return this.get('ajax').request(url).then((json) => json.symbols);
     }
   }
 });
